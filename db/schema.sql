f6a090775a462bc78e2b5155eea9a426fd9e18c0
KYLETCHIU <92279620+KYLETCHIU@users.noreply.github.com>
2022-03-26 21:48:23 -0400

    Connected db
    Connected  database

--- db/schema.sql
+++ db/schema.sql
@@ -1,11 +1,15 @@
 DROP DATABASE IF EXISTS employee_tracker_db;
+
 CREATE DATABASE employee_tracker_db;
+
 USE employee_tracker_db;
+
 CREATE TABLE department (
   id INT AUTO_INCREMENT NOT NULL,
   department_name VARCHAR(30) NULL,
   PRIMARY KEY (id)
 );
+
 CREATE TABLE roles (
   id INT AUTO_INCREMENT NOT NULL,
   title VARCHAR(30) NULL,
@@ -14,6 +18,7 @@ CREATE TABLE roles (
   PRIMARY KEY (id),
   FOREIGN KEY (department_id) REFERENCES department(id)
 );
+
 CREATE TABLE employee (
   id INT AUTO_INCREMENT NOT NULL,
   first_name VARCHAR(30) NULL,
---
+++ utils/connection.js
@@ -0,0 +1,20 @@
+const mysql = require(`mysql`);
+
+//Connect to Database
+var connection = mysql.createConnection({
+
+    host: `localhost`,
+    port: 3306,
+    user: `root`,
+    password: `Dakota2020`,
+    database: `employee_tracker_db`
+});
+
+connection.connect(function (err) {
+
+    if (err) throw err;
+    console.log(`connected as id ` + connection.threadId + `\n`);
+});
+
+module.exports = connection;
+
