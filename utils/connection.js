e537a20ca7dfbf28541d6fe7731480dde073f052
KYLETCHIU <92279620+KYLETCHIU@users.noreply.github.com>
2022-03-27 16:11:43 -0400

    test push

--- utils/connection.js
+++ utils/connection.js
@@ -1,14 +1,14 @@
-const mysql = require("mysql");
+const mysql = require("mysql2");
 
 //Connect to Database
 var connection = mysql.createConnection({
 
-    host: `localhost`,
+    host: `localhost:3306`,
     port: 3306,
     user: `root`,
     password: `Dakota2020`,
-    socketPath: 'mysql-socket-path'/Applications/MAMP/tmp/mysql/mysql.sock/,
-    database: `employee_tracker_db`
+    //socketPath: 'mysql-socket-path'/Applications/MAMP/tmp/mysql/mysql.sock,
+    database: `employee_tracker_db`,
 });
 
 connection.connect(function (err) {
